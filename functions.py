7624a1835435f58e94a5237a81adb2de583c3446
Dipendra.Shekhawat <dipendra.shekhawat@greenorbit.com>
2019-08-28 19:42:36 +0530

    added parameterized function and calling function in a loop

--- functions.py
+++ functions.py
@@ -12,6 +12,25 @@ def createSquare():
     my_turtle.left(90)
     my_turtle.forward(100)
 
+# function call
 createSquare()
-my_turtle.left(100)
-createSquare()
+
+
+# function with parameters
+
+def square(length, angle):
+    my_turtle.forward(length)
+    my_turtle.left(angle)
+    my_turtle.forward(length)
+    my_turtle.left(angle)
+    my_turtle.forward(length)
+    my_turtle.left(angle)
+    my_turtle.forward(length)
+
+square(200, 90)
+
+# calling function in a Loop
+
+for i in range(4):
+    square(50, 90)
+
